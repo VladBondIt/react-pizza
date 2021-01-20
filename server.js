d4856234d87dbae340d3e703ccef1b4749035294
Vlad Bond <foolowin@gmail.com>
2021-01-20 11:08:09 +0300

    fix deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
     static: './build',
 });
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
